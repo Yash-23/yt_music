892058a640039e03ff14c0236860432f93f99e7f
Yash-23 <yash.sakle23@gmail.com>
2021-07-06 12:28:18 +0530

    NavBar

--- src/NavBar.js
+++ src/NavBar.js
@@ -4,6 +4,7 @@ import logo from "./logo.svg";
 import {AppBar, Button, Toolbar} from "@material-ui/core";
 import {makeStyles} from "@material-ui/core";
 import SearchIcon from '@material-ui/icons/Search';
+import MoreVertIcon from '@material-ui/icons/MoreVert';
 
 const useStyles = makeStyles((theme) => ({
     margin: {
@@ -23,28 +24,30 @@ const NavBar = () => {
         <Toolbar>
         <nav >
             <Link to={"/"}>
-                <img src={logo}/>
+                <img src={logo} />
             </Link>
 
                     <Link to={"/"} style={{ textDecoration: 'none'}}>
-                    <Button size="small" style={{border:'none',color:'white'}} className={classes.margin} >
+                    <Button size="small" style={{border:'none',color:'white',fontSize:"medium"}} className={classes.margin} >
                         Home
                     </Button>
                     </Link>
 
 
                 <Link to="/explore" style={{ textDecoration: 'none'}}>
-                    <Button size="small" style={{border:'none',color:'white'}} className={classes.margin}>
+                    <Button size="small" style={{border:'none',color:'white',fontSize:"medium"}} className={classes.margin}>
                         Explore
                     </Button></Link>
 
                 <Link to="/library" style={{ textDecoration: 'none'}}>
-                    <Button size="small" style={{border:'none',color:'white'}} className={classes.margin}>
+                    <Button size="small" style={{border:'none',color:'white', fontSize:"medium"}} className={classes.margin}>
                         Library
                     </Button></Link>
 
         </nav>
             <Button  style={{border:'none',color:'white'}}><SearchIcon></SearchIcon> Search </Button>
+            <MoreVertIcon fontSize={"medium"} style={{ color: "white" }}/>
+            <Button  style={{ color: "black" ,backgroundColor: "white",border:"none"}}>SIGN IN</Button>
         </Toolbar>
         </AppBar >
     )
