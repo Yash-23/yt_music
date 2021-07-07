51437dfcc1ed68171463af4b4143325d64088f01
Yash-23 <yash.sakle23@gmail.com>
2021-07-07 15:23:59 +0530

    SearchBar :)

--- src/Components/SearchBar.js
+++ src/Components/SearchBar.js
@@ -1,11 +1,11 @@
 import {Input} from "@material-ui/core";
-import useStyles from "../Styles/NavBarStyle";
+import useStyles from "../Styles/SearchBarStyle";
 
 const SearchBar = () => {
     const classes = useStyles();
 
     return (
-      <Input color={"primary"} className={classes.searchBar} />
+      <Input type={"text"} color={"primary"} className={classes.searchBar} />
     )
 }
 export default SearchBar;
--- src/Styles/SearchBarStyle.js
+++ src/Styles/SearchBarStyle.js
@@ -1,6 +1,11 @@
 import {makeStyles} from "@material-ui/styles";
 
 export default makeStyles({
-
+    searchBar:{
+        color:"white",
+        backgroundColor: '#212121',
+       padding:'0 56px 0 0' ,
+        height:'48px',
+        width: '804px'
     }
-)
+})
