c8d8a4cfa24776b85d214d86f89b24a572beb379
Anirudh Mitra <34450549+Anirudh4583@users.noreply.github.com>
2021-07-13 12:50:28 +0530

    firebase null

--- src/Service/firebase.js
+++ src/Service/firebase.js
@@ -2,13 +2,13 @@ import firebase from 'firebase';
 
 const firebaseApp = firebase.initializeApp({
   // For Firebase JS SDK v7.20.0 and later, measurementId is optional
-  apiKey: 'AIzaSyBTaOEnFyP-BZxmcDE6NHx5dk0gQ-d8UX8',
-  authDomain: 'react-todo-an.firebaseapp.com',
-  projectId: 'react-todo-an',
-  storageBucket: 'react-todo-an.appspot.com',
-  messagingSenderId: '550422368632',
-  appId: '1:550422368632:web:5351813464bcbe2c9cb88d',
-  measurementId: 'G-P6V9D5G5LB',
+  apiKey: "",
+  authDomain: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: "",
+  measurementId: ""
 });
 
 firebase.analytics();
